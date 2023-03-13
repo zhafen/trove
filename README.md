1c9e0e64e2d69f2f157be2ddd209f8a61ae95cc2
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-03-13 16:16:53 -0700

    Updates for pip version.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # trove
 A minimal package for managing your troves of treasure (data).
 
-Clone and install:
+With pip:
+```
+pip install treasure-trove
+```
+
+Clone from source and install:
 ```
 git clone git@github.com:zhafen/trove.git
 cd trove
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="trove",
+    name="treasure-trove",
     version="0.1",
     author="Zach Hafen-Saavedra",
     author_email="zachary.h.hafen@gmail.com",
